672a93d0f7708cc076f8acbf73999ef7b8934df5
Esperanza Escacena <espemacem@gmail.com>
2019-09-23 19:44:02 +0200

    nav usuario mejorado

--- reservas/src/main/java/com/salesianostriana/reservas/controller/ReservaController.java
+++ reservas/src/main/java/com/salesianostriana/reservas/controller/ReservaController.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.reservas.formbean.FormbeanFecha;
 import com.salesianostriana.reservas.model.Horas;
+import com.salesianostriana.reservas.service.AulaServicio;
 import com.salesianostriana.reservas.service.ReservaServicio;
+import com.salesianostriana.reservas.service.UsuarioServicio;
 
 
 
@@ -21,17 +24,25 @@ import com.salesianostriana.reservas.service.ReservaServicio;
 public class ReservaController {
 	@Autowired
 	ReservaServicio rs;
+	@Autowired
+	AulaServicio as;
+	@Autowired
+	UsuarioServicio us;
 	/**
 	 * Muestra la pantalla para escoger una fecha y luego mostrar la semana
 	 * @param model
 	 * @param p
 	 * @return
 	 */
-	@GetMapping("/user/reservar")
-	public String mostrarReservar(Model model, Principal p) {
-		
+	@GetMapping("/user/reservar/{id}")
+	public String mostrarReservar(@PathVariable("id") long id,Model model, Principal p) {
+		LocalDate fecha=LocalDate.now();
+		model.addAttribute("aula",as.findById(id));
 		model.addAttribute("formbeanFecha",new FormbeanFecha());
-		model.addAttribute("p", p);
+		model.addAttribute("usuario",us.buscarUsuarioLogged(p));
+		model.addAttribute("horas",Horas.values());
+		model.addAttribute("reservas", rs.listarReservasSemana(rs.CalcularSemanasMes(fecha)));
+		model.addAttribute("semana",rs.CalcularSemanasMes(fecha));
 		return "user/reservas";
 	}
 	/**
@@ -40,12 +51,15 @@ public class ReservaController {
 	 * @param model
 	 * @return
 	 */
-	@PostMapping("/user/reservar/semana")
-	public String mostrarReservarSemana(@ModelAttribute("formbeanFecha") FormbeanFecha ff,Model model) {
+	@PostMapping("/user/reservar/{id}/submit")
+	public String mostrarReservarSemana(@PathVariable("id") long id,
+			@ModelAttribute("formbeanFecha") FormbeanFecha ff, Model model, Principal p) {
+		model.addAttribute("aula",as.findById(id));
 		LocalDate fecha=rs.ConversorTextoFecha(ff.getFecha());
 		model.addAttribute("horas",Horas.values());
 		model.addAttribute("reservas", rs.listarReservasSemana(rs.CalcularSemanasMes(fecha)));
 		model.addAttribute("semana",rs.CalcularSemanasMes(fecha));
+		model.addAttribute("usuario",us.buscarUsuarioLogged(p));
 		
 		return "user/reservas";
 	}
