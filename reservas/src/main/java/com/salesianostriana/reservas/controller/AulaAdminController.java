81d15162bdbd28f3891e4f246794dd099fec00f8
Alvaro <alvaromarquez98@gmail.com>
2019-09-24 10:56:57 +0200

    Implementada la posibilidad de aprobar usuarios sin validar o de borrar su petición. Creada clase ReservaAdminController, donde se implementará la historia Admin-Reserva

--- reservas/src/main/java/com/salesianostriana/reservas/controller/AulaAdminController.java
+++ reservas/src/main/java/com/salesianostriana/reservas/controller/AulaAdminController.java
@@ -1,8 +1,22 @@
 package com.salesianostriana.reservas.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AulaAdminController {
+	
+	/**
+	 * OJO A LA ANOTACIÓN DE CLASE "@RequestMapping("/admin")". Todos los @GetMapping 
+	 * 
+	 * que se incluyan en esta clase irán precedidos de "/admin". Es decir, 
+	 * 
+	 * si creas un método anotado con "@GetMapping("/aulas")" la URL para llegar 
+	 * 
+	 * a la plantilla que devuelva NO será "localhost:9000/aulas", sino que será
+	 * 
+	 * "localhost:9000/admin/aulas".
+	 */
 
 }
--- reservas/src/main/java/com/salesianostriana/reservas/controller/ReservaAdminController.java
+++ reservas/src/main/java/com/salesianostriana/reservas/controller/ReservaAdminController.java
@@ -1,13 +1,12 @@
 package com.salesianostriana.reservas.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
-public class ComunAdminController {
-
+public class ReservaAdminController {
+	
 	/**
 	 * OJO A LA ANOTACIÓN DE CLASE "@RequestMapping("/admin")". Todos los @GetMapping 
 	 * 
@@ -20,9 +19,4 @@ public class ComunAdminController {
 	 * "localhost:9000/admin/aulas".
 	 */
 	
-	@GetMapping("/inicio")
-	public String portada() {
-		return "admin/pantalla1";
-	}
-
 }
