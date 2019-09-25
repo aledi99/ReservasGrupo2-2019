6f9b694be1e84c977d824538dc1e0217d1bc7278
Alvaro <alvaromarquez98@gmail.com>
2019-09-25 14:33:20 +0200

    Implementación método para guardar sabados y domingos en una lista implementada en el command line runner

--- reservas/src/main/java/com/salesianostriana/reservas/ReservasApplication.java
+++ reservas/src/main/java/com/salesianostriana/reservas/ReservasApplication.java
@@ -1,5 +1,10 @@
 package com.salesianostriana.reservas;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,6 +32,21 @@ public class ReservasApplication {
 				us.save(usu);
 				System.out.println(usu);
 			}
+			
+			LocalDate hoy = LocalDate.now();
+			int anno = hoy.getYear();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			
+			LocalDate startDate = LocalDate.parse("01/01/" + anno, formatter);
+			LocalDate finishDate = LocalDate.parse("31/12/" + (anno + 1), formatter);
+			
+			List<LocalDate> festivos = new ArrayList<LocalDate>();
+			
+			for(LocalDate date = startDate ; date.isBefore(finishDate) ; date = date.plusDays(1)) {
+				if (date.getDayOfWeek().getValue() == 6 || date.getDayOfWeek().getValue() == 7) {
+					festivos.add(date);
+				}
+			} 
 			
 			
 		};
