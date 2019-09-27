8f8e2b202e0e80d12f3910699e68e246476cdf9e
Esperanza Macarena <espemacem@gmail.com>
2019-09-27 12:20:06 +0200

    he añadido datos al datasql

--- reservas/src/main/resources/data.sql
+++ reservas/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'usuario@usuario.com', '1234', 'Miguel', 'Campos Rivera', false, true);
 insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'admin@admin.com', 'admin', 'Luis Miguel', 'López Magaña', true, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'usuario@usuario.com', '1234', 'Miguel', 'Campos Rivera', false, true);
 insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'kbramall0@nytimes.com', '6syZHFD', 'Kaine', 'Bramall', false, false);
 insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'jpetrus1@discovery.com', 'o5iJqjpXy6', 'Judon', 'Petrus', false, false);
 insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'gstilling2@nature.com', 'kZoN4PUpSy1', 'Gwyn', 'Stilling', false, false);
@@ -10,23 +10,49 @@ insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) 
 insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'rpoultney7@merriam-webster.com', 'UMj3EepXm', 'Ripley', 'Poultney', false, false);
 insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'ctorn8@reverbnation.com', 'O3MgnZ', 'Candace', 'Torn', false, false);
 insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'vbunting9@mashable.com', 'URTzuoj', 'Virgie', 'Bunting', false, false);
-
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'mwooller0@over-blog.com', 'LRCv02SoBMz', 'Moishe', 'Wooller', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'odelieu1@ebay.co.uk', 'mtxsTTWnV', 'Osbourne', 'Delieu', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'dtripp2@studiopress.com', 'YmWL4Z4dX', 'Dorisa', 'Tripp', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'cfriatt3@live.com', 'd1tPvixl', 'Christoph', 'Friatt', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'gbullene4@google.pl', 'b9wLKf44', 'Guinna', 'Bullene', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'rwalewicz5@istockphoto.com', '7iiVpeb2uMw', 'Riva', 'Walewicz', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'lorbon6@qq.com', 'A0hAd2', 'Legra', 'Orbon', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'afogel7@rambler.ru', 'M3mvdyHrd', 'Arley', 'Fogel', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'dmolfino8@europa.eu', 'j6YShAmrch', 'Diane', 'Molfino', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'bwalsham9@addtoany.com', 'e4fZ8p1qDSdG', 'Brandais', 'Walsham', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'mguinerya@addtoany.com', '6GwmdvUAS3rH', 'Mendie', 'Guinery', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'msweatlandb@example.com', 'iOifj3XOLT', 'Madelina', 'Sweatland', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'ebackc@redcross.org', 'Z5Xqtb14lu', 'Eleanor', 'Back', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'bklugmand@angelfire.com', 'fdFTFKm', 'Burke', 'Klugman', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'eandrie@fema.gov', 'FHHyKpnt', 'Emilio', 'Andri', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'cclewlowf@arstechnica.com', 'INJCN1YG5P', 'Constantine', 'Clewlow', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'vclemptong@ft.com', 'k6PtuZavvE', 'Valli', 'Clempton', false, false);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'bguesth@blinklist.com', '7ODrkLw', 'Batsheva', 'Guest', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'ggrevesoni@patch.com', 'hn2GwNu', 'Guthrie', 'Greveson', false, true);
+insert into USUARIO (id, email, password, nombre, apellidos, admin, gestionado) values (NEXTVAL('hibernate_sequence'), 'alittlerj@ning.com', 'aRNNoxSEB6', 'Axe', 'Littler', false, false);
 insert into AULA (id, nombre) values (NEXTVAL('hibernate_sequence'), '1º DAM');
 insert into AULA (id, nombre) values (NEXTVAL('hibernate_sequence'), '2º DAM');
 insert into AULA (id, nombre) values (NEXTVAL('hibernate_sequence'), 'Biblioteca');
 insert into AULA (id, nombre) values (NEXTVAL('hibernate_sequence'), 'Laboratorio');
 insert into AULA (id, nombre) values (NEXTVAL('hibernate_sequence'), '1º AyF');
 insert into AULA (id, nombre) values (NEXTVAL('hibernate_sequence'), '1º Comercio');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 0, 33, 2 ,'2019-09-27');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 34, 2 ,'2019-09-27');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 35, 2 ,'2019-09-24');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 4, 33, 3 ,'2019-09-27');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 1, 33, 3 ,'2019-09-25');	
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 0, 34, 3 ,'2019-09-27');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 3, 34, 1 ,'2019-09-27');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 3, 34, 4 ,'2019-09-26');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 3, 34, 5 ,'2019-09-24');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 35, 6 ,'2019-09-27');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 2, 35, 7 ,'2019-09-27');	
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 2, 36, 7 ,'2019-09-28');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 0, 36, 7 ,'2019-09-28');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 33, 7 ,'2019-10-02');
+insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 33, 7 ,'2019-10-01');	
 
 
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 0, 13, 1 ,'2019-09-27');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 14, 1 ,'2019-09-27');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 15, 1 ,'2019-09-24');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 4, 13, 1 ,'2019-09-27');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 1, 13, 1 ,'2019-09-25');	
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 0, 14, 1 ,'2019-09-27');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 3, 14, 1 ,'2019-09-27');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 3, 14, 1 ,'2019-09-26');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 3, 14, 1 ,'2019-09-24');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 5, 15, 1 ,'2019-09-27');
-insert into RESERVA (id, hora, aula_id, usuario_id, fecha) values (NEXTVAL('hibernate_sequence'), 2, 15, 1 ,'2019-09-27');	
+
+
+
