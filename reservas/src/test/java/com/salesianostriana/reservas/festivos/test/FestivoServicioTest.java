048f7f80f996b981b18147ff88b86cebc7ad03f2
Alvaro <alvaromarquez98@gmail.com>
2019-10-28 00:57:09 +0100

    Implementados los tests de FestivoServicio
    No he sido capaz de implementar un test del método GuardarSabadosYDomingos

---
+++ reservas/src/test/java/com/salesianostriana/reservas/festivos/test/FestivoServicioTest.java
@@ -0,0 +1,115 @@
+package com.salesianostriana.reservas.festivos.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.salesianostriana.reservas.model.Festivo;
+import com.salesianostriana.reservas.repository.FestivoRepository;
+import com.salesianostriana.reservas.service.FestivoServicio;
+
+@SpringBootTest
+public class FestivoServicioTest {
+
+	@InjectMocks
+	private FestivoServicio fs;
+	
+	@Mock
+	private FestivoRepository fr;
+	
+	@Before
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public final void testBuscarSabadosYDomingos() {
+		List<LocalDate> lista = fs.BuscarSabadosYDomingos();
+		List<DayOfWeek> sabYDom = new ArrayList<>();
+		sabYDom.add(DayOfWeek.SATURDAY);
+		sabYDom.add(DayOfWeek.SUNDAY);
+		
+		assertFalse(lista.isEmpty());
+		
+		for (LocalDate d : lista) {
+			assertThat(sabYDom.contains(d.getDayOfWeek()));
+		}
+	}
+	
+	@Test
+	public final void testBuscarSabados() {
+		List<LocalDate> lista = fs.BuscarSabados();
+		List<DayOfWeek> sab = new ArrayList<>();
+		sab.add(DayOfWeek.SATURDAY);
+
+		assertFalse(lista.isEmpty());
+		
+		for (LocalDate d : lista) {
+			assertThat(sab.contains(d.getDayOfWeek()));
+		}
+	}
+	
+	@Test
+	public final void testBuscarDomingos() {
+		List<LocalDate> lista = fs.BuscarDomingos();
+		List<DayOfWeek> dom = new ArrayList<>();
+		dom.add(DayOfWeek.SUNDAY);
+
+		assertFalse(lista.isEmpty());
+		
+		for (LocalDate d : lista) {
+			assertThat(dom.contains(d.getDayOfWeek()));
+		}
+	}
+	
+	@Test
+	public final void testComprobarFestivo() {
+		List<Festivo> festivos = new ArrayList<>();
+		Festivo f = new Festivo(1L, LocalDate.now(), true);
+		festivos.add(f);
+		when(fr.findAll()).thenReturn(festivos);
+		
+		assertThat(fs.comprobarFestivo(LocalDate.now()));
+		
+		assertFalse(fs.comprobarFestivo(LocalDate.of(1900, 1, 1)));
+	}
+	
+	@Test
+	public final void testListarFestivosSinFinesDeSemana() {
+		List<Festivo> festivos = new ArrayList<>();
+		Festivo listar = new Festivo(1L, LocalDate.now(), true);
+		Festivo noListar = new Festivo(2L, LocalDate.now(), false);
+		festivos.add(listar);
+		festivos.add(noListar);
+		
+		when(fr.findAll()).thenReturn(festivos);
+		
+		List<Festivo> resultados = fs.listarFestivosSinFinesDeSemana();
+		
+		assertThat(resultados.contains(listar));
+		
+		assertFalse(resultados.contains(noListar));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
