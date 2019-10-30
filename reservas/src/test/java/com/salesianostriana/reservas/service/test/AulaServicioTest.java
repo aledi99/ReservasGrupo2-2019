1abf694e84fefca77768fabf2d28816394e4ebd2
jmterrero <josetg999@gmail.com>
2019-10-30 09:53:35 +0100

    arreglo tests eliminarAula

--- reservas/src/test/java/com/salesianostriana/reservas/service/test/AulaServicioTest.java
+++ reservas/src/test/java/com/salesianostriana/reservas/service/test/AulaServicioTest.java
@@ -4,10 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -18,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.salesianostriana.reservas.model.Aula;
 import com.salesianostriana.reservas.repository.AulaRepository;
 import com.salesianostriana.reservas.service.AulaServicio;
+import com.salesianostriana.reservas.service.ReservaServicio;
 
 @SpringBootTest
 public class AulaServicioTest {
@@ -28,23 +25,21 @@ public class AulaServicioTest {
 	@Mock
 	private AulaRepository ar;
 	
+	@Mock
+	private ReservaServicio rs;
+	
 	@BeforeEach
 	public void initMocks() {
 		MockitoAnnotations.initMocks(this);
 	}
 	
-	private String nombreAula = "Aula 1";
-
 	@Test
 	public void testEliminarAula() {
 		Aula aulaMock = new Aula();
 		
-		List<Aula> mockList = Arrays.asList(aulaMock);
-		
-		// when(ar.findAula(aulaMock)).thenReturn(mockList);
 		as.eliminarAula(aulaMock);
 		
-		verify(ar, times(1)).deleteAll(mockList);
+		verify(ar, times(1)).delete(aulaMock);
 	}
 	
 	@Test
